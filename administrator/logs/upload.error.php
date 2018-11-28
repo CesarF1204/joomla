bbaedfda7cdbb4200d4c63238583913666b9b646
unknown <ite15-15@M306.UC-BCF.EDU.PH>
2018-11-28 09:17:36 +0800

    Initialized repository

---
+++ administrator/logs/upload.error.php
@@ -0,0 +1,21 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2018-11-24 01:03:22 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2018-11-24T01:03:22+00:00	INFO ::1	upload	
+2018-11-24T01:04:29+00:00	INFO ::1	upload	
+2018-11-24T01:05:04+00:00	INFO ::1	upload	
+2018-11-24T01:05:55+00:00	INFO ::1	upload	
+2018-11-24T01:06:25+00:00	INFO ::1	upload	
+2018-11-24T01:07:42+00:00	INFO ::1	upload	
+2018-11-24T01:08:33+00:00	INFO ::1	upload	
+2018-11-24T01:08:59+00:00	INFO ::1	upload	
+2018-11-24T01:09:36+00:00	INFO ::1	upload	
+2018-11-24T01:10:18+00:00	INFO ::1	upload	
+2018-11-24T01:10:54+00:00	INFO ::1	upload	
+2018-11-24T01:11:42+00:00	INFO ::1	upload	
+2018-11-24T01:12:08+00:00	INFO ::1	upload	
+2018-11-24T01:12:44+00:00	INFO ::1	upload	
+2018-11-24T01:13:11+00:00	INFO ::1	upload	
